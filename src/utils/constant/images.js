c1761cd87c50467a5272f8f602379481503ef04f
nkp1111 <neerajp4321@gmail.com>
2023-04-23 15:51:22 +0530

    feat: add react-icons and sidebar content

---
+++ src/utils/constant/images.js
@@ -0,0 +1,9 @@
+import logo from '../../assets/logo.svg'
+import bottle from '../../assets/source.svg'
+import cart from '../../assets/undraw_shopping_app_flsj 1.svg'
+
+export default {
+  logo,
+  bottle,
+  cart,
+}
