ccc8d845c569ce8c71c370e155c2e4d332081b9f
nkp1111 <neerajp4321@gmail.com>
2023-04-26 15:01:46 +0530

    feat: add cart item detail component

--- src/component/SidebarItem/CartItemDetail.js
+++ src/component/SidebarItem/CartItemDetail.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
-
 import BackButton from '../BackButton'
 
 const CartItemDetail = ({ item, setShowItemDetail }) => {
 
-  const { name, id, category, image, description } = item
+  const { name, category, image, description } = item
   const handleItemDetail = () => {
     setShowItemDetail({ show: false, item: null })
   }
@@ -13,7 +12,16 @@ const CartItemDetail = ({ item, setShowItemDetail }) => {
   return (
     <div className='main__sidebar-details'>
       <BackButton function1={handleItemDetail} />
-      {name}
+      <article className="details-item d-flex flex-column">
+        <div className="order-2">
+          <h3>{name}</h3>
+          <p className='category'>{category}</p>
+          <p>{description}</p>
+        </div>
+        <div className="order-1">
+          <img src={image} alt={`${name}`} />
+        </div>
+      </article>
     </div>
   )
 }
