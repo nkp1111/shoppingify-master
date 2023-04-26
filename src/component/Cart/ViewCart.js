17c8b9f0e5f5f27346e25ed4a236a64aa2338031
nkp1111 <neerajp4321@gmail.com>
2023-04-26 11:34:02 +0530

    refactor: viewCart component from index file

---
+++ src/component/Cart/ViewCart.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react'
+
+import { images } from '../../utils'
+import SearchForm from './SearchForm'
+import CartItems from './CartItems'
+import useGlobalContext from '../../context'
+
+const ViewCart = () => {
+
+  const [showEdit, setShowEdit] = useState(false);
+  const { state, shoppingEnded } = useGlobalContext()
+  const emptyCart = state?.cart?.items?.length === 0
+
+  useEffect(() => {
+    if (emptyCart) {
+      setShowEdit(false)
+    }
+  }, [emptyCart]);
+
+  return (
+    <div className={`cart d-flex flex-column align-items-center h-100`}>
+      <div className='cart-header d-flex'>
+        <div className="cart-content order-2">
+          <h2>Didn’t find what you need?</h2>
+          <button className='btn'>Add item</button>
+        </div>
+        <div className="cart-image">
+          <img src={images.bottle} alt="bottle" />
+        </div>
+      </div>
+
+      {emptyCart
+        ? <div className='empty-cart-display position-relative'>
+          <span>No items</span>
+          <img
+            src={images.cart}
+            alt="lady with cart"
+            className='position-absolute' />
+        </div>
+        : <CartItems state={state} showEdit={showEdit} setShowEdit={setShowEdit} />}
+
+      <div className="cart-footer mt-auto">
+        {showEdit
+          ? (
+            <div className='btn-holder d-flex'>
+              <button className="btn"
+                onClick={() => shoppingEnded("cancelled")}>cancel</button>
+              <button className="btn"
+                onClick={() => shoppingEnded("completed")}>Complete</button>
+            </div>
+          )
+          : <SearchForm empty={emptyCart} />}
+      </div>
+    </div>
+  )
+}
+
+export default ViewCart
--- src/component/Cart/index.js
+++ src/component/Cart/index.js
@@ -1,54 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { images } from '../../utils'
-import SearchForm from './SearchForm'
-import CartItems from './CartItems'
-import "./cart.css"
 import useGlobalContext from '../../context'
+import ViewCart from './ViewCart'
+import "./cart.css"
+import CartItemDetail from './CartItemDetail'
 
 const Index = () => {
 
-  const [showEdit, setShowEdit] = useState(false);
-  const { state, shoppingEnded } = useGlobalContext()
-  const emptyCart = state?.cart?.items?.length === 0
+  const [showAddItemForm, setShowAddItemForm] = useState(false);
+  const { showItemDetail, setShowItemDetail } = useGlobalContext()
 
   return (
     <section className='main__sidebar-cart h-100'>
-      <div className={`cart d-flex flex-column align-items-center h-100`}>
-        <div className='cart-header d-flex'>
-          <div className="cart-content order-2">
-            <h2>Didn’t find what you need?</h2>
-            <button className='btn'>Add item</button>
-          </div>
-          <div className="cart-image">
-            <img src={images.bottle} alt="bottle" />
-          </div>
-        </div>
-
-        {emptyCart
-          ? <div className='empty-cart-display position-relative'>
-            <span>No items</span>
-            <img
-              src={images.cart}
-              alt="lady with cart"
-              className='position-absolute' />
-          </div>
-          : <CartItems state={state} showEdit={showEdit} setShowEdit={setShowEdit} />}
-
-        <div className="cart-footer mt-auto">
-          {showEdit
-            ? (
-              <div className='btn-holder d-flex'>
-                <button className="btn"
-                  onClick={() => shoppingEnded("cancelled")}>cancel</button>
-                <button className="btn"
-                  onClick={() => shoppingEnded("completed")}>Complete</button>
-              </div>
-            )
-            : <SearchForm empty={emptyCart} />}
-
-        </div>
-      </div>
+      {showAddItemForm
+        ? <form>form</form>
+        : showItemDetail.show
+          ? <CartItemDetail item={showItemDetail.item} setShowItemDetail={setShowItemDetail} />
+          : <ViewCart />}
     </section>
   )
 }
