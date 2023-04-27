ce7921210977433de444a7cb747a53a42a807fcb
nkp1111 <neerajp4321@gmail.com>
2023-04-27 18:51:54 +0530

    feat: add fixed mainSidebar on small screen

--- src/pages/Sidebar/index.js
+++ src/pages/Sidebar/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { RiShoppingCart2Line } from 'react-icons/ri'
 import { Link } from 'react-router-dom'
 
@@ -9,6 +9,34 @@ import "./sidebar.css"
 
 const Index = () => {
   const { state } = useGlobalContext()
+
+  const handleMainSidebar = () => {
+    // on small screen change main sidebar display setting
+    const width = window.innerWidth
+    if (width <= 960) {
+      const mainRightSidebar = document.querySelector(".app__main-sidebar")
+      const displayStyle = window.getComputedStyle(mainRightSidebar, null).display
+      if (displayStyle === "none") {
+        mainRightSidebar.style.display = "block"
+      } else {
+        mainRightSidebar.style.display = "none"
+      }
+    }
+  }
+
+  const resetRightSidebar = () => {
+    // show main right side bar on big screen
+    if (window.innerWidth > 960) {
+      const mainRightSidebar = document.querySelector(".app__main-sidebar")
+      mainRightSidebar.style.display = "block"
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", resetRightSidebar)
+    return () => window.removeEventListener("resize", resetRightSidebar)
+  })
+
   return (
     <div className='app__sidebar'>
       <nav className="navbar">
@@ -24,7 +52,7 @@ const Index = () => {
           <SidebarNav />
 
           {/* cart items  */}
-          <div className='app__sidebar-cart'>
+          <div className='app__sidebar-cart' onClick={handleMainSidebar}>
             <p className="item-num">{state.cart.items.length}</p>
             <div>
               <RiShoppingCart2Line className='cart-icon' />
