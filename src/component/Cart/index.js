8796630326e0452c87b1593233df2cec4cca2fc4
nkp1111 <neerajp4321@gmail.com>
2023-04-27 12:25:42 +0530

    refactor: main right sidebar padding

--- src/component/Cart/index.js
+++ src/component/Cart/index.js
@@ -21,28 +21,30 @@ const Cart = () => {
 
   return (
     <div className='main__sidebar-cart d-flex flex-column align-items-center h-100'>
-      <div className='cart-header d-flex'>
-        <div className="cart-content order-2">
-          <h2>Didn’t find what you need?</h2>
-          <button className='btn bg-white'
-            onClick={() => setShowAddItemForm(true)}>Add item</button>
-        </div>
-        <div className="cart-image">
-          <img src={images.bottle} alt="bottle" />
-        </div>
-      </div>
-
-      {emptyCart
-        ? (
-          <div className='empty-cart-display position-relative'>
-            <span>No items</span>
-            <img
-              src={images.cart}
-              alt="lady with cart"
-              className='position-absolute' />
+      <div className='cart-padding w-100 d-flex flex-column align-items-center'>
+        <div className='cart-header d-flex'>
+          <div className="cart-content order-2">
+            <h2>Didn’t find what you need?</h2>
+            <button className='btn bg-white'
+              onClick={() => setShowAddItemForm(true)}>Add item</button>
           </div>
-        )
-        : <CartItems state={state} showEdit={showEdit} setShowEdit={setShowEdit} searchTerm={searchTerm} />}
+          <div className="cart-image">
+            <img src={images.bottle} alt="bottle" />
+          </div>
+        </div>
+
+        {emptyCart
+          ? (
+            <div className='empty-cart-display position-relative'>
+              <span>No items</span>
+              <img
+                src={images.cart}
+                alt="lady with cart"
+                className='position-absolute' />
+            </div>
+          )
+          : <CartItems state={state} showEdit={showEdit} setShowEdit={setShowEdit} searchTerm={searchTerm} />}
+      </div>
 
       <div className="cart-footer mt-auto w-100">
         {showEdit
